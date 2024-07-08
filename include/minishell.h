0d32403eb83f0c595e69c2476af125534b6e446a
Catharina Linggadjaja <clinggad@c1b1c3.42berlin.de>
2024-07-08 15:30:03 +0200

    added allowed headers to file

--- include/minishell.h
+++ include/minishell.h
@@ -3,18 +3,39 @@
 /*                                                        :::      ::::::::   */
 /*   minishell.h                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: dmusulas <dmusulas@student.42berlin.de>    +#+  +:+       +#+        */
+/*   By: clinggad <clinggad@student.42berlin.de>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/05 17:32:12 by dmusulas          #+#    #+#             */
-/*   Updated: 2024/07/05 17:35:34 by dmusulas         ###   ########.fr       */
+/*   Updated: 2024/07/08 14:45:50 by clinggad         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-//TODO: Include libs that are allowed to use
+
 //NOTE: probably will split this header file into multiple ones for tidyness
 //later
 
 #ifndef MINISHELL_H 
 # define MINISHELL_H
-# include <stdio.h>
+
+/* HEADERS */
+#include <stdio.h> // printf
+#include <stdlib.h> // malloc, free, exit, getenv
+#include <unistd.h> // read, write, access, open, close, fork,
+				// getcwd, chdir, unlink, execve, dup, dup2, pipe, isatty
+				// ttyname, ttyslot
+#include <sys/wait.h> // wait, waitpid, wait3, wait4
+#include <sys/types.h> // fork, stat, lstat, fstat, opendir, readdir, closedir
+//#include <dirent.h> // opendir, readir, closedir
+//#include <sys/stat.h> // stat, lstat, fstat
+#include <signal.h> // signal, igaction, sigemptyset, sigaddset, kill
+#include <readline/readline.h> // readline
+#include <readline/history.h> // rl_clear_history, rl_on_new_line, rl_replace_line
+						// rl_redisplay, add_history
+#include <termios.h> // tcstattr, tcgetattr
+#include <term.h> // tgetent, tgetflag, tgetnum, tgetstr, tgoto, tputs
+#include <erno.h> // sterror, perror
+#include <sys/ioctl.h> // ioctl
+
+
+
 #endif
