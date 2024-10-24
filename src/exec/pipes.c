787838a1fdfe564318a0bcdafa99e5da16b3e6d6
Peter Moelzer <moelzerpeter@gmail.com>
2024-10-24 12:35:55 +0200

    Added exit status and wrong exit when empty argument for pipes. TODO: correct exit status from pipes

--- src/exec/pipes.c
+++ src/exec/pipes.c
@@ -6,7 +6,7 @@
 /*   By: pmolzer <pmolzer@student.42berlin.de>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/14 20:31:14 by dmusulas          #+#    #+#             */
-/*   Updated: 2024/10/21 15:20:14 by pmolzer          ###   ########.fr       */
+/*   Updated: 2024/10/24 12:31:18 by pmolzer          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -38,7 +38,7 @@ void	handle_pipe_child(int *fd, int *fd_in, t_ast *node, t_tools *tools)
 		close(*fd_in);
 	}
 	execute_command(node->left, tools);
-	exit(1);
+	exit(tools->last_exit_status);  // Changed from exit(1) to exit with proper status
 }
 
 void	handle_pipes(t_ast *node, t_tools *tools)
@@ -47,6 +47,7 @@ void	handle_pipes(t_ast *node, t_tools *tools)
 	pid_t	pid;
 	int		fd_in;
 	t_ast	*current_node;
+	int		status;
 
 	fd_in = -1;
 	current_node = node;
@@ -63,11 +64,21 @@ void	handle_pipes(t_ast *node, t_tools *tools)
 			handle_pipe_parent(fd, &fd_in);
 		current_node = current_node->right;
 	}
-	if (current_node->token == T_REDIR_IN)
-		set_infile(current_node, tools);
+	pid = fork();
+	if (pid == 0)
+	{
+		if (current_node->token == T_REDIR_IN)
+			set_infile(current_node, tools);
+		if (fd_in != -1)
+			dup2(fd_in, STDIN_FILENO);
+		execute_command(current_node, tools);
+		exit(tools->last_exit_status);
+	}
 	if (fd_in != -1)
-		dup2(fd_in, STDIN_FILENO);
-	execute_command(current_node, tools);
-	while (wait(NULL) > 0)
-		;
+		close(fd_in);
+	while (wait(&status) > 0)
+	{
+		if (WIFEXITED(status))
+			tools->last_exit_status = WEXITSTATUS(status);
+	}
 }
