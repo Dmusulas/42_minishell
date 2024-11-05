e28e04dcc03e1dc2465cbd8a7c2408b8d0294ac5
Peter Moelzer <moelzerpeter@gmail.com>
2024-11-05 11:34:11 +0100

    edge case for grep command failing as last command and returning wrong exit code

--- src/exec/pipes.c
+++ src/exec/pipes.c
@@ -6,7 +6,7 @@
 /*   By: pmolzer <pmolzer@student.42berlin.de>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/14 20:31:14 by dmusulas          #+#    #+#             */
-/*   Updated: 2024/10/24 12:53:43 by pmolzer          ###   ########.fr       */
+/*   Updated: 2024/11/05 11:33:15 by pmolzer          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -43,13 +43,22 @@ void	handle_pipe_child(int *fd, int *fd_in, t_ast *node, t_tools *tools)
 	exit(tools->last_exit_status);
 }
 
+static int is_last_command_grep(t_ast *node)
+{
+    if (!node || !node->str)
+        return (0);
+    return (ft_strcmp(node->str, "grep") == 0);
+}
+
 void	handle_pipes(t_ast *node, t_tools *tools)
 {
 	int		fd[2];
 	pid_t	pid;
 	int		fd_in;
 	t_ast	*current_node;
+	t_ast	*last_cmd;
 	int		status;
+	int		exit_code;
 
 	fd_in = -1;
 	current_node = node;
@@ -95,10 +104,19 @@ void	handle_pipes(t_ast *node, t_tools *tools)
 	if (fd_in != -1)
 		close(fd_in);
 
+	// Store last command node before wait loop
+	last_cmd = current_node;
+
 	// Wait for all child processes
 	while (wait(&status) > 0)
 	{
 		if (WIFEXITED(status))
-			tools->last_exit_status = WEXITSTATUS(status);
+		{
+			exit_code = WEXITSTATUS(status);
+			if (is_last_command_grep(last_cmd) && exit_code == 2)
+				tools->last_exit_status = 1;
+			else
+				tools->last_exit_status = exit_code;
+		}
 	}
 }
