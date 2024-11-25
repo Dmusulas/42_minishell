8408498a5479e6caf99f34af254cf84c15cd4d6a
Peter Moelzer <moelzerpeter@gmail.com>
2024-11-25 15:01:06 +0100

    signals improvement

--- src/signals.c
+++ src/signals.c
@@ -6,58 +6,65 @@
 /*   By: pmolzer <pmolzer@student.42berlin.de>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/02 16:01:22 by dmusulas          #+#    #+#             */
-/*   Updated: 2024/11/25 14:55:21 by pmolzer          ###   ########.fr       */
+/*   Updated: 2024/11/25 15:00:48 by pmolzer          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
-void	sigint_handler_active(int signal)
+// Handles signals in interactive mode, specifically SIGINT, by printing a newline,
+// clearing the current line, and redisplaying the prompt.
+static void	handle_active_signal(int signal)
 {
-	write(STDOUT_FILENO, "\n", 1);
-	rl_replace_line("", 0);
-	rl_on_new_line();
-	rl_redisplay();
-	(void)signal;
+	if (signal == SIGINT)
+	{
+		write(STDOUT_FILENO, "\n", 1);
+		rl_replace_line("", 0);
+		rl_on_new_line();
+		rl_redisplay();
+	}
 }
 
-void	sigint_handler_inactive(int signal)
+// Handles signals in non-interactive mode, responding to SIGINT by printing a newline
+// and to SIGQUIT by printing a "Quit" message along with the signal number.
+static void	handle_inactive_signal(int signal)
 {
-	write(STDOUT_FILENO, "\n", 1);
-	(void)signal;
+	if (signal == SIGINT)
+		write(STDOUT_FILENO, "\n", 1);
+	else if (signal == SIGQUIT)
+	{
+		ft_putstr_fd("Quit: ", STDERR_FILENO);
+		ft_putnbr_fd(signal, STDERR_FILENO);
+		ft_putchar_fd('\n', STDERR_FILENO);
+	}
 }
 
-void	sigquit_handler_inactive(int signal)
+// Sets up signal handling by defining the handler for SIGINT and SIGQUIT signals
+// using the provided handler function.
+static void	setup_signals(void (*handler)(int))
 {
-	ft_putstr_fd("Quit: ", STDERR_FILENO);
-	ft_putnbr_fd(signal, STDERR_FILENO);
-	ft_putchar_fd('\n', STDERR_FILENO);
+	struct sigaction	sa;
+
+	sa.sa_handler = handler;
+	sigemptyset(&sa.sa_mask);
+	sa.sa_flags = 0;
+	sigaction(SIGINT, &sa, NULL);
+	sigaction(SIGQUIT, &sa, NULL);
 }
 
+// Configures the signal handling for active mode by setting the active signal handler.
 void	set_active_signals(void)
 {
-	struct sigaction	sa;
-
-	sa.sa_handler = sigint_handler_active;
-	sigemptyset(&sa.sa_mask);
-	sa.sa_flags = 0;
-	sigaction(SIGINT, &sa, NULL);
-	sa.sa_handler = SIG_IGN;
-	sigaction(SIGQUIT, &sa, NULL);
+	setup_signals(handle_active_signal);
 }
 
+// Configures the signal handling for inactive mode by setting the inactive signal handler.
 void	set_inactive_signals(void)
 {
-	struct sigaction	sa;
-
-	sa.sa_handler = sigint_handler_inactive;
-	sigemptyset(&sa.sa_mask);
-	sa.sa_flags = 0;
-	sigaction(SIGINT, &sa, NULL);
-	sa.sa_handler = sigquit_handler_inactive;
-	sigaction(SIGQUIT, &sa, NULL);
+	setup_signals(handle_inactive_signal);
 }
 
+// Initializes the signal handling by setting the active signal handler.
 void	init_signals(void)
 {
 	set_active_signals();
