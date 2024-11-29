56679dee7abfb71d9d1b1e9d42fc3c2695b67ccf
Peter Moelzer <moelzerpeter@gmail.com>
2024-11-29 18:04:06 +0100

    refactored function tokenize_input

--- src/lex/lexer.c
+++ src/lex/lexer.c
@@ -6,21 +6,21 @@
 /*   By: pmolzer <pmolzer@student.42berlin.de>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/02 16:01:05 by dmusulas          #+#    #+#             */
-/*   Updated: 2024/11/29 17:38:32 by pmolzer          ###   ########.fr       */
+/*   Updated: 2024/11/29 18:01:35 by pmolzer          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
-
-static char *ft_strjoin_free(char *s1, char *s2)
+static char	*ft_strjoin_free(char *s1, char *s2)
 {
-    char *result;
-    
-    result = ft_strjoin(s1, s2);
-    free(s1);
-    return (result);
+	char	*result;
+
+	result = ft_strjoin(s1, s2);
+	free(s1);
+	return (result);
 }
+
 /**
  * Checks if the given character is a token delimiter ('|', '<', or '>').
  *
@@ -49,8 +49,8 @@ int	handle_q_arg(char *s, int start, t_tools *tools)
 	int		i;
 	char	quote;
 	char	*arg;
-	char    *temp;
-	int     total_len;
+	char	*temp;
+	int		total_len;
 
 	i = start;
 	total_len = 0;
@@ -71,7 +71,7 @@ int	handle_q_arg(char *s, int start, t_tools *tools)
 		i++;
 		total_len = i - start;
 		if (s[i] != '"' && s[i] != '\'')
-			break;
+			break ;
 	}
 	add_tk(&(tools->lexer_lst), make_tk(arg, T_ARG));
 	return (total_len);
@@ -103,77 +103,113 @@ int	handle_arg(char *s, int start, t_tools *tools)
 	return (i - start);
 }
 
-static void handle_space_token(t_tools *tools, int *i, int *start, bool *is_cmd)
+static void	handle_final_token(t_tools *tools, int start, int i, bool is_cmd)
 {
-    if (*i > *start)
-    {
-        char *token = ft_substr(tools->args, *start, *i - *start);
-        add_tk(&(tools->lexer_lst), make_tk(token, *is_cmd ? T_CMD : T_ARG));
-        *is_cmd = false;
-    }
-    *start = *i + 1;
-    (*i)++;
+	char	*token;
+
+	if (i > start)
+	{
+		token = ft_substr(tools->args, start, i - start);
+		if (is_cmd)
+			add_tk(&(tools->lexer_lst), make_tk(token, T_CMD));
+		else
+			add_tk(&(tools->lexer_lst), make_tk(token, T_ARG));
+	}
 }
 
-static void handle_special_token(t_tools *tools, int *i, int *start, bool *is_cmd)
+static void	handle_space_token(t_tools *tools, int *i, int *start, bool *is_cmd)
 {
-    int offset;
+	char	*token;
 
-    if (*i > *start)
-    {
-        char *token = ft_substr(tools->args, *start, *i - *start);
-        add_tk(&(tools->lexer_lst), make_tk(token, *is_cmd ? T_CMD : T_ARG));
-    }
-    offset = ft_two_tk(tools->args[*i], tools->args[*i + 1], tools);
-    if (offset > 0)
-    {
-        *i += offset;
-        *start = *i;
-        *is_cmd = false;
-        return;
-    }
-    ft_one_tk(tools->args[*i], tools);
-    (*i)++;
-    *start = *i;
-    *is_cmd = (tools->args[*i - 1] == '|');
+	if (*i > *start)
+	{
+		token = ft_substr(tools->args, *start, *i - *start);
+		if (*is_cmd)
+			add_tk(&(tools->lexer_lst), make_tk(token, T_CMD));
+		else
+			add_tk(&(tools->lexer_lst), make_tk(token, T_ARG));
+		*is_cmd = false;
+	}
+	*start = *i + 1;
+	(*i)++;
 }
 
-int tokenize_input(t_tools *tools)
+static void	handle_special_token(t_tools *tools, int *i,
+		int *start, bool *is_cmd)
 {
-    int     i;
-    int     start;
-    bool    is_cmd;
-    char    quote_type;
+	int		offset;
+	char	*token;
 
-    i = 0;
-    start = 0;
-    is_cmd = true;
-    quote_type = '\0';
-    tools->lexer_lst = NULL;
+	if (*i > *start)
+	{
+		token = ft_substr(tools->args, *start, *i - *start);
+		if (*is_cmd)
+			add_tk(&(tools->lexer_lst), make_tk(token, T_CMD));
+		else
+			add_tk(&(tools->lexer_lst), make_tk(token, T_ARG));
+	}
+	offset = ft_two_tk(tools->args[*i], tools->args[*i + 1], tools);
+	if (offset > 0)
+	{
+		*i += offset;
+		*start = *i;
+		*is_cmd = false;
+		return ;
+	}
+	ft_one_tk(tools->args[*i], tools);
+	(*i)++;
+	*start = *i;
+	*is_cmd = (tools->args[*i - 1] == '|');
+}
 
-    while (tools->args[i])
-    {
-        if ((tools->args[i] == '"' || tools->args[i] == '\'') && quote_type == '\0')
-            quote_type = tools->args[i];
-        else if (tools->args[i] == quote_type)
-            quote_type = '\0';
-        
-        if (tools->args[i] == ' ' && quote_type == '\0')
-        {
-            handle_space_token(tools, &i, &start, &is_cmd);
-            continue;
-        }
-        if (quote_type == '\0' && check_tk(tools->args[i]))
-        {
-            handle_special_token(tools, &i, &start, &is_cmd);
-            continue;
-        }
-        i++;
-    }
-    if (i > start)
-    {
-        char *token = ft_substr(tools->args, start, i - start);
-        add_tk(&(tools->lexer_lst), make_tk(token, is_cmd ? T_CMD : T_ARG));
-    }
-    return (quote_type == '\0' ? 1 : ft_error(ERR_SYNTAX, tools));
+static int	process_lexer_tokens(char *input_str, int *i, char *quote_type)
+{
+	if ((input_str[*i] == '"' || input_str[*i] == '\'') && *quote_type == '\0')
+		*quote_type = input_str[*i];
+	else if (input_str[*i] == *quote_type)
+		*quote_type = '\0';
+	return (1);
+}
+
+static int	handle_token_processing(t_tools *tools, bool *is_cmd)
+{
+	int		i;
+	int		start;
+	char	quote_type;
+
+	i = 0;
+	start = 0;
+	quote_type = '\0';
+	while (tools->args[i])
+	{
+		process_lexer_tokens(tools->args, &i, &quote_type);
+		if (tools->args[i] == ' ' && quote_type == '\0')
+		{
+			handle_space_token(tools, &i, &start, is_cmd);
+			continue ;
+		}
+		if (quote_type == '\0' && check_tk(tools->args[i]))
+		{
+			handle_special_token(tools, &i, &start, is_cmd);
+			continue ;
+		}
+		i++;
+	}
+	handle_final_token(tools, start, i, *is_cmd);
+	if (quote_type == '\0')
+		return (1);
+	return (0);
+}
+
+int	tokenize_input(t_tools *tools)
+{
+	bool	is_cmd;
+	int		result;
+
+	is_cmd = true;
+	tools->lexer_lst = NULL;
+	result = handle_token_processing(tools, &is_cmd);
+	if (!result)
+		return (ft_error(ERR_SYNTAX, tools));
+	return (1);
 }
