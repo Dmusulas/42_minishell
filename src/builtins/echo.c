014f04f1f9d222c315ccf94ae1aa3cb0e1c8d21e
Peter Moelzer <moelzerpeter@gmail.com>
2024-11-29 18:41:36 +0100

    refactored echo print function

--- src/builtins/echo.c
+++ src/builtins/echo.c
@@ -6,42 +6,45 @@
 /*   By: pmolzer <pmolzer@student.42berlin.de>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/12 22:33:09 by pmolzer           #+#    #+#             */
-/*   Updated: 2024/11/05 13:23:09 by pmolzer          ###   ########.fr       */
+/*   Updated: 2024/11/29 18:39:29 by pmolzer          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
-static void	print_argument(t_ast *current, t_tools *tools)
+static void	print_argument_with_expansion(t_ast *current, t_tools *tools)
 {
 	char	*str;
 	int		i;
 
-	if (!tools->in_single_quotes && ft_strcmp(current->str, "$?") == 0)
-		ft_putnbr_fd(tools->last_exit_status, STDOUT_FILENO);
-	else
+	str = current->str;
+	i = 0;
+	if (!tools->in_single_quotes)
 	{
-		str = current->str;
-		i = 0;
-		if (!tools->in_single_quotes)
+		while (str[i])
 		{
-			while (str[i])
+			if (str[i] == '$' && str[i + 1] == '?')
 			{
-				if (str[i] == '$' && str[i + 1] == '?')
-				{
-					ft_putnbr_fd(tools->last_exit_status, STDOUT_FILENO);
-					i += 2;
-				}
-				else
-					ft_putchar_fd(str[i++], STDOUT_FILENO);
+				ft_putnbr_fd(tools->last_exit_status, STDOUT_FILENO);
+				i += 2;
 			}
-		}
-		else
-		{
-			while (str[i])
+			else
 				ft_putchar_fd(str[i++], STDOUT_FILENO);
 		}
 	}
+	else
+	{
+		while (str[i])
+			ft_putchar_fd(str[i++], STDOUT_FILENO);
+	}
+}
+
+static void	print_argument(t_ast *current, t_tools *tools)
+{
+	if (!tools->in_single_quotes && ft_strcmp(current->str, "$?") == 0)
+		ft_putnbr_fd(tools->last_exit_status, STDOUT_FILENO);
+	else
+		print_argument_with_expansion(current, tools);
 }
 
 int	ft_echo(t_ast *cmd_node, t_tools *tools)
