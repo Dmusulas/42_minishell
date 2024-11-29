6c9b81109e3ab9804127556c12eeeffa8cdec34b
Peter Moelzer <moelzerpeter@gmail.com>
2024-11-29 17:32:04 +0100

    exit 2

--- src/builtins/exit.c
+++ src/builtins/exit.c
@@ -6,7 +6,7 @@
 /*   By: pmolzer <pmolzer@student.42berlin.de>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/23 11:02:53 by pmolzer           #+#    #+#             */
-/*   Updated: 2024/11/05 13:21:22 by pmolzer          ###   ########.fr       */
+/*   Updated: 2024/11/29 17:31:51 by pmolzer          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,7 +25,7 @@ static long long	get_exit_code(char *arg, t_tools *tools)
 	if (ft_is_not_integer(arg))
 	{
 		ft_error(ERR_NUMERIC_ARG_REQUIRED, tools);
-		exit(255);
+		exit(2); // should be this 2 or 255? Tester with "exit hello" requires 2
 	}
 	return (ft_atoi(arg));
 }
